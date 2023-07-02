a7f6c099756bfd29e4d55920f18e32073c9164f3
Lev <0xdeadfae@gmail.com>
2023-07-02 17:25:44 +0400

    path validation

--- contract/src/MerkleMountainRange.sol
+++ contract/src/MerkleMountainRange.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.8.13;
 contract MerkleMountainRange {
 
     struct enumeratedCap{
-        uint8 cap_id;
+        uint8 cap_depth;
         uint248 cap_hash;
     }
 
+    struct merklePathEntry{
+        uint248 element;
+        bool selector;
+    }
 
     uint8 caps_length;
     mapping(uint8 => enumeratedCap) caps;
 
-    function push(uint248 update) public {
+    function push(uint248 update) internal {
         
         uint8 _caps_length = caps_length;
 
@@ -27,8 +31,8 @@ contract MerkleMountainRange {
         _caps_length -= 1;
         while(true){
             cap = caps[_caps_length];
-            if (cap.cap_id == _update.cap_id){
-                _update = enumeratedCap(_update.cap_id + 1, uint248(uint256((keccak256(abi.encodePacked(cap.cap_hash, _update.cap_hash))))));
+            if (cap.cap_depth == _update.cap_depth){
+                _update = enumeratedCap(_update.cap_depth + 1, uint248(uint256((keccak256(abi.encodePacked(cap.cap_hash, _update.cap_hash))))));
                 if (_caps_length == 0){
                     caps[0] = _update;
                     caps_length = 1;
@@ -44,6 +48,33 @@ contract MerkleMountainRange {
         }
     }
 
+    function validate(uint256 id, uint248 leaf, uint248[] calldata path) internal view {
+        uint256 length = path.length;
+        uint8 _caps_length = caps_length;
+        uint248 l;
+        uint248 r;
+        for(uint256 i = 0; i < _caps_length ; i++) {
+            
+            enumeratedCap memory tmp = caps[uint8(i)];
 
+            if (id >= (1 << tmp.cap_depth)){
+                id -= (1<<tmp.cap_depth);
+            } else {
+                require(length == tmp.cap_depth, "path has incorrect length");
+                l = leaf;
+                for (uint256 j = 0; j < path.length; j++){
+                    r = path[j];
+                    if ( 1 == ((id<<j) & 1) ) {
+                        (l, r) = (r, l);
+                    }
+                    l = uint248(uint256((keccak256(abi.encodePacked(l, r)))));
+                }
+                require(l == tmp.cap_hash);
+                return;
+            }
+
+            require(false, "id out of range");
+        }
+    }
 
 }
