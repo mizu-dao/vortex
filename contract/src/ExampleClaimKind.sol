6d7212be2661adcd6bf2dfd70bac875a0ba620cb
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-09-22 16:53:32 +0400

    chore: rename ClaimKind to IClaimKind

--- contract/src/ExampleClaimKind.sol
+++ contract/src/ExampleClaimKind.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.19;
 
-import "./ClaimKind.sol";
+import "./IClaimKind.sol";
 import "./Router.sol";
 
-contract ExampleClaimKind is ClaimKind {
+contract ExampleClaimKind is IClaimKind {
     uint8 constant UNDEFINED_STATUS_CODE = 2;
     uint8 constant NO_BLESSER_STATUS_CODE = 3;
     uint8 constant EXPIRED_STATUS_CODE = 4;
--- contract/src/IClaimKind.sol
+++ contract/src/IClaimKind.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.19;
 
-interface ClaimKind {
+interface IClaimKind {
     function depositClaim(uint256[] calldata args, uint256 poolId) external returns (uint32, uint256, uint256);
 
     function status(uint256 claimId) external view returns (uint8);
